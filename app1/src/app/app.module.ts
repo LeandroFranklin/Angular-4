309f2f2b84808e33a74e89de4a01d201f9074a36
Leandro Franklin <leandrofrankss5@gmail.com>
2023-12-11 20:34:39 -0300

    App1 - Criando o componente Painel via CLI

--- app1/src/app/app.module.ts
+++ app1/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { TopoComponent } from './topo/topo.component';
+import { PainelComponent } from './painel/painel.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopoComponent
+    TopoComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule,
