5bf377b5c21eb122da497a18369befbf19eee9f1
Leandro Franklin <leandrofrankss5@gmail.com>
2023-12-30 08:26:20 -0300

    App1 - Topo(Template)

--- app1/src/app/topo/topo.component.ts
+++ app1/src/app/topo/topo.component.ts
@@ -1,6 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+
 @Component({
-    selector: ' app-topo',
-    templateUrl: "./topo.component.html"
+  selector: 'app-topo',
+  templateUrl: './topo.component.html',
+  styleUrl: './topo.component.css'
 })
-export class TopoComponent{}
+export class TopoComponent {
+
+}
