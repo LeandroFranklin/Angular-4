d3c1d18fabb37de2834d862776f9de634d6c2d8d
Leandro Franklin <leandrofrankss5@gmail.com>
2024-01-01 13:54:19 -0300

    App1 - Estabelecendo a lógica das rodadas de frases

--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -14,7 +14,13 @@ export class PainelComponent {
   public frases: Frase[] = FRASES;
   public resposta: string = "";
 
-  constructor(){console.log(this.frases)}
+  public rodada: number = 0;
+  public rodadaFrase: Frase; 
+
+  constructor(){
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.frases)
+  }
 
   public atualizarResposta(resposta:Event): void{
     this.resposta = ((<HTMLInputElement>resposta.target).value);
